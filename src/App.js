1ad2b8f68c3b1992b518bc0563a1f9717223291c
Wophe <wophe.akinlade@gmail.com>
2025-02-27 22:27:46 -0400

    Added a header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Calendar from "./components/Calendar/Calendar";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Calendar />
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <u>
+        <h1>Your Calendar for 2022</h1>
+      </u>
+    </div>
+  );
+};
+
+export default Header;
